c0696c81a3fdbd3be26820c75d0fff09b02082a3
Prosper Atu <atu.prosper@gmail.com>
2021-08-27 04:41:51 +0100

    added formspree to handle the contact form

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,12 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "./contact.scss";
 import { UilMessage } from "@iconscout/react-unicons";
 import { data } from "./data";
+import { useForm, ValidationError } from "@formspree/react";
 
 function Contact({ themeIcon }) {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  // Initialize Formspree (A third party app for sending emails), set your formspree endpoint inside the useForm()
+  const [state, handleSubmit] = useForm("xnqloeyg");
 
   return (
     <section className="contact section" id="contact">
@@ -44,32 +44,60 @@ function Contact({ themeIcon }) {
               <label htmlFor="name" className="contactLabel">
                 Name
               </label>
-              <input type="text" id="name" />
+              <input type="text" id="name" name="name" required />
+              <ValidationError
+                prefix="Name"
+                field="name"
+                errors={state.errors}
+              />
             </div>
 
-            <div className={themeIcon ? "contactContentDark" : "contactContent"}>
+            <div
+              className={themeIcon ? "contactContentDark" : "contactContent"}
+            >
               <label htmlFor="email" className="contactLabel">
                 Email
               </label>
-              <input type="email" id="email" />
+              <input type="email" id="email" name="email" required />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
             </div>
           </div>
           <div className={themeIcon ? "contactContentDark" : "contactContent"}>
             <label htmlFor="project" className="contactLabel">
               Subject
             </label>
-            <input type="text" id="project" />
+            <input type="text" id="project" name="subject" required />
+            <ValidationError
+              prefix="Subject"
+              field="subject"
+              errors={state.errors}
+            />
           </div>
           <div className={themeIcon ? "contactContentDark" : "contactContent"}>
             <label htmlFor="message" className="contactLabel">
               Message
             </label>
-            <textarea name="" id="message" cols="0" rows="7"></textarea>
+            <textarea name="message" id="message" cols="0" rows="7" required />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
           </div>
 
-          <button type="submit">
+          <button type="submit" disabled={state.submitting}>
             Submit Message <UilMessage size="18" className="messageIcon" />
           </button>
+
+          {state.succeeded && <p>Thanks for your meesage!</p>}
+          {state.succeeded &&
+            setTimeout(() => {
+              location.reload();
+            }, 1500)}
         </form>
       </div>
     </section>
